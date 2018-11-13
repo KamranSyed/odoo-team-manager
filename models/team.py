3e0d72b4091af601a1e252abeba4d0011a2bea6a
Kamran Syed <lovelykamran@gmail.com>
2018-11-13 11:37:06 +0500

    Modified model to add managers

--- models/team.py
+++ models/team.py
@@ -6,5 +6,8 @@ class team(models.Model):
 	 _name = 'ksteam.team'
 
 	 name = fields.Char(required=True)
-	 description = fields.Char()
-	 employee_ids = fields.Many2many('hr.employee', string='Employees')
+	 description 	= fields.Char()
+	 employee_ids 	= fields.Many2many('hr.employee', string='Employees')
+	 manager_ids 	= fields.Many2many('hr.employee', string='Managers')
+	 active			= fields.Boolean('Active?', default=True)
+	 
