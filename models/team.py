ff6e098a7cd4e2601454583804af285c01354aae
Kamran Syed <lovelykamran@gmail.com>
2018-11-13 12:23:37 +0500

    Changed label from Employees to Members.

--- models/team.py
+++ models/team.py
@@ -7,7 +7,7 @@ class team(models.Model):
 
 	 name = fields.Char(required=True)
 	 description 	= fields.Char()
-	 employee_ids 	= fields.Many2many('hr.employee', string='Employees')
+	 employee_ids 	= fields.Many2many('hr.employee', string='Members')
 	 manager_ids 	= fields.Many2many('hr.employee', 'ksteam_team_managers_rel', string='Managers')
 	 active			= fields.Boolean('Active?', default=True)
 	 
